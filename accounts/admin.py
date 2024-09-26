2b93c9deb676ca8e77dcd6a5b0c5d149252a8146
Thras Papadopoulos <thras.papa@gmail.com>
2024-09-26 22:08:15 +0300

    Created custom user forms, add the forms at admin site and users test

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = get_user_model()
+    list_display = [
+        "email",
+        "username",
+        "is_superuser",
+    ]
+    add_fieldsets = (
+            (
+                None,
+                {
+                    "fields": (
+                        "email",
+                        "username",
+                        "password1",
+                        "password2",
+                    ),
+                },
+            ),
+        )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+    )
+
+admin.site.register(get_user_model(), CustomUserAdmin)
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+
+class CustomUserTest(TestCase):
+    def test_add_user(self):
+        user = get_user_model().objects.create_user(
+            username='user1',
+            email='user1@mail.com',
+            password='pass123!'
+        )
+        self.assertEqual(user.username, 'user1')
+        self.assertEqual(user.email, 'user1@mail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            username='user1',
+            email='user1@mail.com',
+            password='pass123!'
+        )
+        self.assertEqual(user.username, 'user1')
+        self.assertEqual(user.email, 'user1@mail.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+    
