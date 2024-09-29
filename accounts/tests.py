c4026e5129393301c53156aaf79fcf8fc28b5826
Thras Papadopoulos <thras.papa@gmail.com>
2024-09-29 23:58:57 +0300

    Registration, update accounts app, templates new and update olds, tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
+
 # Create your tests here.
 
 
@@ -28,3 +32,22 @@ class CustomUserTest(TestCase):
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
     
+class SignUpTest(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Error by me!')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
