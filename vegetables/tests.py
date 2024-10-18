697b9331c15d0bdf25bad8a055862b845917edeb
thras <thras.papa@gmail.com>
2024-10-18 19:16:22 +0300

    permissions @list & @detail views, update & add tests

--- vegetables/tests.py
+++ vegetables/tests.py
@@ -1,12 +1,18 @@
 from django.test import TestCase
 from django.urls import reverse
 from .models import Vegetable
-
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
 class VegetableTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="dummyUser",
+            email="dummyUser@email.com",
+            password="test1234"
+        )
+        
         cls.vegetable = Vegetable.objects.create(
             name = "Tomato",
             calorie = "13",
@@ -22,13 +28,24 @@ class VegetableTests(TestCase):
         self.assertEqual(f"{self.vegetable.category}", "Fruit")
         self.assertEqual(f"{self.vegetable.wiki}", "www.testurl.com")
 
-    def test_vegetable_list_view(self):
+    def test_vegetable_list_view_login(self):
+        self.client.login(username="dummyUser", password="test1234")
         response = self.client.get(reverse("vegetable_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Tomato")
         self.assertTemplateUsed(response, "vegetables/vegetable_list.html")
 
+    def test_vegetable_list_view_logout(self):
+        self.client.logout()
+        response = self.client.get(reverse("vegetable_list"))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+        response, "%s?next=/vegetables/" % (reverse("login")))
+        response = self.client.get("%s?next=/vegetables/" % (reverse("login")))
+        self.assertContains(response, "Log In")       
+
     def test_vegetable_detail_view(self):
+        self.client.login(username="dummyUser", password="test1234")
         response = self.client.get(self.vegetable.get_absolute_url())
         no_response = self.client.get("/vegetables/12345/")
         self.assertEqual(response.status_code, 200)
--- vegetables/views.py
+++ vegetables/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
 from .models import Vegetable
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-class VegetableListView(ListView):
+class VegetableListView(LoginRequiredMixin, ListView):
     model = Vegetable
     paginate_by = 8
     context_object_name = 'vegetable_list'
     template_name = 'vegetables/vegetable_list.html'
+    login_url = "login"
 
 
-class VegetableDetailView(DetailView):
+class VegetableDetailView(LoginRequiredMixin, DetailView):
     model = Vegetable
     context_object_name = "vegetable"
     template_name = 'vegetables/vegetable_detail.html'
+    login_url = "login"
