209504e39b5a13e684e3653ca82d02cbc5ecf0d0
thras <thras.papa@gmail.com>
2024-10-20 23:28:06 +0300

    search view and template plus fix pagination issue at search query, add vegetable form to add new and view

--- vegetables/urls.py
+++ vegetables/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import VegetableListView, VegetableDetailView
+from .views import VegetableListView, VegetableDetailView, SearchViewList, AddVegetable
 
 urlpatterns = [
     path('', VegetableListView.as_view(), name = 'vegetable_list'),
     path('<uuid:pk>/', VegetableDetailView.as_view(), name='vegetable_detail'),
+    path('search/', SearchViewList.as_view(), name='search' ),
+    path('add/', AddVegetable.as_view(), name='add_vegetable'),
 ]
--- vegetables/views.py
+++ vegetables/views.py
@@ -1,6 +1,9 @@
 from .models import Vegetable
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.shortcuts import render
+from django.urls import reverse
 # Create your views here.
 
 class VegetableListView(LoginRequiredMixin, ListView):
@@ -16,3 +19,28 @@ class VegetableDetailView(LoginRequiredMixin, DetailView):
     context_object_name = "vegetable"
     template_name = 'vegetables/vegetable_detail.html'
     login_url = "login"
+
+class SearchViewList(ListView):
+    model = Vegetable
+    paginate_by = 8
+    context_object_name = 'vegetable_list'
+    template_name = 'vegetables/search.html'
+
+    def querystring(self):
+        query = self.request.GET.copy()
+        query.pop(self.page_kwarg, None)
+        return query.urlencode()
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Vegetable.objects.filter(
+            Q(name__icontains = query)|Q(category__icontains = query)
+        )
+
+class AddVegetable(CreateView):
+  ''' View to add a new Vehicle.'''
+  model = Vegetable
+  fields = '__all__'
+  context_object_name = 'add_vegetable'
+  def get_success_url(self):
+    return reverse('vegetable_list')
